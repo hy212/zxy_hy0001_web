a6bda429950e66b7b85ae9d0ee325eb03f3772e1
xyinghu <xyinghu@tencent.com>
2024-03-14 19:59:30 +0800

    feat: update demo

--- src/App.vue
+++ src/App.vue
@@ -26,16 +26,28 @@ export default {
     return {
       baas: null,
       users: [],
-      value: ''
+      value: '',
+      myTableObject: null,
     };
   },
   async created() {
+    // sdk初始化
     this.baas = require('minapp-sdk');
     this.baas.init('84c4ee23bba6240808a2', {});
+    // 用户登录
     await this.baas.auth.login({username: 'huying', password: 'huying'}).then(user => {
       console.log(111, user)
     });
+    // 获取记录列表
     await this.getRecordList();
+    // 调用云函数
+    this.baas.invoke('func_test').then(res => {
+      console.log('云函数返回结果', res.data);
+    })
+    // this.realTimeDatabase();
+  },
+  unmounted() {
+    this.myTableObject.unsubscribe(); // 取消订阅上述示例中数据表的新增数据动作
   },
   methods: {
     /** 获取数据列表 */
@@ -53,6 +65,26 @@ export default {
       console.log(res);
       this.value = '';
       await this.getRecordList();
+    },
+    /** 实时数据库功能测试 */
+    realTimeDatabase() {
+      try {
+        this.myTableObject = new this.baas.TableObject('test');
+        this.myTableObject.subscribe('create', {
+            oninit: () => {
+              console.log(`订阅成功==>`)
+            },
+            onevent: res => {
+              console.log(`订阅推送==>`, res)
+              this.getRecordList();
+            },
+            onerror: err => {
+              console.log(`订阅断开==>`, err)
+            },
+          })
+      } catch (e) {
+        console.log(e.toString());
+      }
     },
   },
 }
