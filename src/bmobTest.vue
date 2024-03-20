a251b7315cd90965593975fbd171f2ef10c4ddb5
xyinghu <xyinghu@tencent.com>
2024-03-20 17:07:23 +0800

    feat: add bmob test

---
+++ src/bmobTest.vue
@@ -0,0 +1,52 @@
+<template>
+  <!--  <img alt="Vue logo" src="assets/logo.png">-->
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+ 授权页面
+
+</template>
+
+<script>
+import Bmob from "hydrogen-js-sdk";
+
+export default {
+  name: 'BmobTest',
+  components: {
+  },
+  data() {
+    return {
+      baas: null,
+      users: [],
+      value: '',
+      myTableObject: null,
+    };
+  },
+  async created() {
+    console.log(111);
+    Bmob.initialize("aefae693045cacfc", "f84d78e84386aa91635eb04b0cf2df31");
+    let params = {
+      username: 'huying',
+      password: 'huying',
+    }
+    await Bmob.User.register(params);
+    Bmob.User.login(params.username, params.password).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    });
+  },
+  unmounted() {
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,43 @@ import ZxyTest from "@/ZxyTest.vue";
 import WxTest from "@/WxTest.vue";
 import { createRouter, createWebHistory } from 'vue-router';
 import zxyAuth from "@/ZxyAuth.vue";
+import bmobTest from "@/bmobTest.vue";
 const routes = [
-    { path: '/', redirect: '/zxy' },
-    { path: '/zxy', component: ZxyTest },
-    { path: '/zxyAuth', component: zxyAuth },
-    { path: '/wx', component: WxTest }
+    {
+        path: '/',
+        redirect: '/zxy',
+        meta: {
+            title: '知晓云SDK测试页面'
+        },
+    },
+    {
+        path: '/zxy',
+        component: ZxyTest,
+        meta: {
+            title: '知晓云SDK测试页面'
+        },
+    },
+    {
+        path: '/zxyAuth',
+        component: zxyAuth,
+        meta: {
+            title: '知晓云SDK登录授权测试页面（待跑通）'
+        },
+    },
+    {
+        path: '/wx',
+        component: WxTest,
+        meta: {
+            title: '微信云开发SDK测试'
+        },
+    },
+    {
+        path: '/bmob',
+        component: bmobTest,
+        meta: {
+            title: 'bmob测试页面'
+        },
+    }
 ]
 
 
